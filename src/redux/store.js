9ceb0f961bb182c3f05ca9ff08b59b9e33d6a6ad
cristiangarzon22 <86244196+cristiangarzon22@users.noreply.github.com>
2023-06-12 21:19:47 -0500

    new commit

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,16 +1,39 @@
 import { configureStore } from '@reduxjs/toolkit';
 import { persistStore } from 'redux-persist';
-import persistConfig from './persistConfig';
+
 import { contactReducer } from './addContacts/addContacts';
 import persistReducer from 'redux-persist/es/persistReducer';
+import storage from 'redux-persist/lib/storage';
+import {
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+} from 'redux-persist';
+
+
+
+const persistConfig = {
+  key: 'contacts',
+  storage,
+  blacklist: ['filter'],
+};
+
+const persistedContactsReducer = persistReducer(persistConfig, contactReducer);
 
 export const store = configureStore({
   reducer: {
-    contacts: persistReducer(persistConfig, contactReducer),
-   
+    contacts: persistedContactsReducer,
+  },
+  middleware(getDefaultMiddleware) {
+    return getDefaultMiddleware({
+      serializableCheck: {
+        ignoreActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+    });
   },
 });
 
 export const persistor = persistStore(store);
-
-
